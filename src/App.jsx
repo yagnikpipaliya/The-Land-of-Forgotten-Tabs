2936952977cabf6803e8ea3a537fa7bcb21d8eda
yagnikpipaliya-fablead <yagnik.fablead@gmail.com>
2025-01-11 12:40:22 +0530

    Add React Router for navigation and update project title

--- src/App.jsx
+++ src/App.jsx
@@ -23,157 +23,187 @@ import {
 } from "@/components/ui/menubar"
 import { ArrowUpRight, EllipsisVertical } from 'lucide-react'
 import HeroSection from './components/custom/HeroSection'
+import { Route, Routes } from 'react-router-dom'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='w-full h-screen mx-auto text-center'>
-        <MenuBar />
-        <HeroSection />
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resource" element={<Content />} />
+      </Routes>
+
+
     </>
   )
 }
 
 export default App
 
+
+const Layout = ({ children }) => {
+  return (
+    <div className="w-full h-screen mx-auto text-center">
+      <MenuBar />
+      {children}
+    </div>
+  );
+};
+
+
+const Home = () => {
+  return (
+    <>
+      <Layout>
+        <HeroSection />
+      </Layout>
+
+      {/* <div className='w-full h-screen mx-auto text-center'>
+        <MenuBar />
+        <HeroSection />
+      </div> */}
+    </>
+  )
+}
 const Content = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='container mx-auto'>
-        <div class="grid  grid-rows-2 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {/* grid-flow-col */}
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <Button size="csm" >Check <ArrowUpRight /></Button>
-              <Menubar className="absolute right-1 top-3 rounded-md border-0 w-fit bg-transparent">
-                <MenubarMenu>
-                  <MenubarTrigger className="bg-transparent p-0"><EllipsisVertical /></MenubarTrigger>
-                  <MenubarContent>
-                    <MenubarItem>
-                      New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-                    </MenubarItem>
-                    <MenubarItem>
-                      New Window <MenubarShortcut>⌘N</MenubarShortcut>
-                    </MenubarItem>
-                    <MenubarItem disabled>New Incognito Window</MenubarItem>
-                    <MenubarSeparator />
-                    <MenubarSub>
-                      <MenubarSubTrigger>Share</MenubarSubTrigger>
-                      <MenubarSubContent>
-                        <MenubarItem>Email link</MenubarItem>
-                        <MenubarItem>Messages</MenubarItem>
-                        <MenubarItem>Notes</MenubarItem>
-                      </MenubarSubContent>
-                    </MenubarSub>
-                    <MenubarSeparator />
-                    <MenubarItem>
-                      Print... <MenubarShortcut>⌘P</MenubarShortcut>
-                    </MenubarItem>
-                  </MenubarContent>
-                </MenubarMenu>
-              </Menubar>
-              <br />
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-            </SpotlightCard>
+      <Layout>
+        <div className='container mx-auto'>
+          <div class="grid  grid-rows-2 grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            {/* grid-flow-col */}
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <Button size="csm" >Check <ArrowUpRight /></Button>
+                <Menubar className="absolute right-1 top-3 rounded-md border-0 w-fit bg-transparent">
+                  <MenubarMenu>
+                    <MenubarTrigger className="bg-transparent p-0"><EllipsisVertical /></MenubarTrigger>
+                    <MenubarContent>
+                      <MenubarItem>
+                        New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                      </MenubarItem>
+                      <MenubarItem>
+                        New Window <MenubarShortcut>⌘N</MenubarShortcut>
+                      </MenubarItem>
+                      <MenubarItem disabled>New Incognito Window</MenubarItem>
+                      <MenubarSeparator />
+                      <MenubarSub>
+                        <MenubarSubTrigger>Share</MenubarSubTrigger>
+                        <MenubarSubContent>
+                          <MenubarItem>Email link</MenubarItem>
+                          <MenubarItem>Messages</MenubarItem>
+                          <MenubarItem>Notes</MenubarItem>
+                        </MenubarSubContent>
+                      </MenubarSub>
+                      <MenubarSeparator />
+                      <MenubarItem>
+                        Print... <MenubarShortcut>⌘P</MenubarShortcut>
+                      </MenubarItem>
+                    </MenubarContent>
+                  </MenubarMenu>
+                </Menubar>
+                <br />
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="csm">Learn more</Button>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="sm">Learn more</Button>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="sm">Learn more</Button>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="sm">Learn more</Button>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="sm">Learn more</Button>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="sm">Learn more</Button>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="sm">Learn more</Button>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="sm">Learn more</Button>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="sm">Learn more</Button>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="sm">Learn more</Button>
+              </SpotlightCard>
+            </div>
+            <div>
+              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                <i class="fa fa-lock"></i>
+                <h2>Enhanced Security</h2>
+                <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
+                <Button size="sm">Learn more</Button>
+              </SpotlightCard>
+            </div>
           </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="csm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="sm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="sm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="sm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="sm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="sm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="sm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="sm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="sm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="sm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-          <div>
-            <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
-              <i class="fa fa-lock"></i>
-              <h2>Enhanced Security</h2>
-              <p>Our state of the art software offers peace of mind through the strictest security measures.</p>
-              <Button size="sm">Learn more</Button>
-            </SpotlightCard>
-          </div>
-        </div>
-        {/* <Content />
+          {/* <Content />
           <Content /> */}
-      </div >
+        </div >
+      </Layout>
     </>
   )
 }
--- src/components/custom/MenuBar.jsx
+++ src/components/custom/MenuBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Menubar,
     MenubarCheckboxItem,
@@ -14,8 +14,12 @@ import {
     MenubarSubTrigger,
     MenubarTrigger,
 } from "@/components/ui/menubar"
+import { Link, NavLink, useLocation } from 'react-router-dom';
 
 const MenuBar = () => {
+    const [isHomeActive, setIsHomeActive] = useState(null)
+    const [isResourceActive, setIsResourceActive] = useState(null)
+    const location = useLocation(); 
     return (
         <>
             <div className="sticky top-0 z-50 w-full backdrop-blur-md  border-transparent transition-colors bg-transparent supports-backdrop-blur:bg-white/60">{/* bg-white/5 border-white/5  border-b*/}
@@ -23,8 +27,25 @@ const MenuBar = () => {
                     <div className="max-w-screen-lg "> {/* w-full */}
                         <Menubar className="w-full rounded-md bg-white">
                             <MenubarMenu>
-                                <MenubarTrigger className="bg-transparent">File</MenubarTrigger>
-                                <MenubarContent>
+                                {/* <NavLink to={'/'} className={({ isActive }) =>
+                                    isActive ? setIsHomeActive('Home') : setIsHomeActive(null)
+                                }
+                                ><MenubarTrigger className={`${isHomeActive ? 'bg-black text-white' : 'text-black'} hover:border-black`}>Home</MenubarTrigger></NavLink> */}
+
+                                <Link to="/">
+                                    <MenubarTrigger
+                                        className={`${location.pathname === '/'
+                                                ? 'bg-black text-white hover:border-black'
+                                                : 'text-black bg-white hover:border-black hover:border focus:outline-0 focus-within::border-0'
+                                            } outline-offset-0 data-[state=open]:!bg-black data-[state=open]:!text-white`}
+                                            // focus:bg-black focus:text-white
+                                    >
+                                        Home
+                                    </MenubarTrigger>
+                                </Link>
+
+
+                                {/* <MenubarContent>
                                     <MenubarItem>
                                         New Tab <MenubarShortcut>⌘T</MenubarShortcut>
                                     </MenubarItem>
@@ -45,11 +66,25 @@ const MenuBar = () => {
                                     <MenubarItem>
                                         Print... <MenubarShortcut>⌘P</MenubarShortcut>
                                     </MenubarItem>
-                                </MenubarContent>
+                                </MenubarContent> */}
                             </MenubarMenu>
                             <MenubarMenu>
-                                <MenubarTrigger>Edit</MenubarTrigger>
-                                <MenubarContent>
+                                {/* <NavLink to={'/resource'} className={({ isActive }) =>
+                                    isActive ? setIsResourceActive('Resource') : setIsResourceActive(null)
+                                }><MenubarTrigger className={`${isResourceActive ? 'bg-black text-white' : 'text-black'} hover:border-black active:bg-slate-800`}>Edit</MenubarTrigger></NavLink> */}
+                                <Link to="/resource">
+                                    <MenubarTrigger
+                                        className={`${location.pathname === '/resource'
+                                                ? 'bg-black text-white'
+                                                : 'text-black bg-white  focus:outline-0 focus-within::border-0'
+                                            } hover:border-black hover:border outline-offset-0 data-[state=open]:!bg-black data-[state=open]:!text-white`}
+                                            // focus:bg-black focus:text-white
+                                    >
+                                        Edit
+                                    </MenubarTrigger>
+                                </Link>
+
+                                {/* <MenubarContent>
                                     <MenubarItem>
                                         Undo <MenubarShortcut>⌘Z</MenubarShortcut>
                                     </MenubarItem>
@@ -71,9 +106,9 @@ const MenuBar = () => {
                                     <MenubarItem>Cut</MenubarItem>
                                     <MenubarItem>Copy</MenubarItem>
                                     <MenubarItem>Paste</MenubarItem>
-                                </MenubarContent>
+                                </MenubarContent> */}
                             </MenubarMenu>
-                            <MenubarMenu>
+                            {/* <MenubarMenu>
                                 <MenubarTrigger>View</MenubarTrigger>
                                 <MenubarContent>
                                     <MenubarCheckboxItem>
@@ -108,7 +143,7 @@ const MenuBar = () => {
                                     <MenubarSeparator />
                                     <MenubarItem inset>Add Profile...</MenubarItem>
                                 </MenubarContent>
-                            </MenubarMenu>
+                            </MenubarMenu> */}
                         </Menubar>
                     </div>
                 </div>
@@ -117,107 +152,4 @@ const MenuBar = () => {
     );
 };
 
-
-// const MenuBar = () => {
-//     return (
-//         <>
-
-//             <div className="sticky top-0 z-50 w-full backdrop-blur-md bg-white/20 border-b border-white/10">
-//                 <div className="container mx-auto flex justify-center py-4">
-
-//                     <Menubar className="w-fit max-w-screen-lg rounded-md">
-//                         <MenubarMenu>
-//                             <MenubarTrigger>File</MenubarTrigger>
-//                             <MenubarContent>
-//                                 <MenubarItem>
-//                                     New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-//                                 </MenubarItem>
-//                                 <MenubarItem>
-//                                     New Window <MenubarShortcut>⌘N</MenubarShortcut>
-//                                 </MenubarItem>
-//                                 <MenubarItem disabled>New Incognito Window</MenubarItem>
-//                                 <MenubarSeparator />
-//                                 <MenubarSub>
-//                                     <MenubarSubTrigger>Share</MenubarSubTrigger>
-//                                     <MenubarSubContent>
-//                                         <MenubarItem>Email link</MenubarItem>
-//                                         <MenubarItem>Messages</MenubarItem>
-//                                         <MenubarItem>Notes</MenubarItem>
-//                                     </MenubarSubContent>
-//                                 </MenubarSub>
-//                                 <MenubarSeparator />
-//                                 <MenubarItem>
-//                                     Print... <MenubarShortcut>⌘P</MenubarShortcut>
-//                                 </MenubarItem>
-//                             </MenubarContent>
-//                         </MenubarMenu>
-//                         <MenubarMenu>
-//                             <MenubarTrigger>Edit</MenubarTrigger>
-//                             <MenubarContent>
-//                                 <MenubarItem>
-//                                     Undo <MenubarShortcut>⌘Z</MenubarShortcut>
-//                                 </MenubarItem>
-//                                 <MenubarItem>
-//                                     Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
-//                                 </MenubarItem>
-//                                 <MenubarSeparator />
-//                                 <MenubarSub>
-//                                     <MenubarSubTrigger>Find</MenubarSubTrigger>
-//                                     <MenubarSubContent>
-//                                         <MenubarItem>Search the web</MenubarItem>
-//                                         <MenubarSeparator />
-//                                         <MenubarItem>Find...</MenubarItem>
-//                                         <MenubarItem>Find Next</MenubarItem>
-//                                         <MenubarItem>Find Previous</MenubarItem>
-//                                     </MenubarSubContent>
-//                                 </MenubarSub>
-//                                 <MenubarSeparator />
-//                                 <MenubarItem>Cut</MenubarItem>
-//                                 <MenubarItem>Copy</MenubarItem>
-//                                 <MenubarItem>Paste</MenubarItem>
-//                             </MenubarContent>
-//                         </MenubarMenu>
-//                         <MenubarMenu>
-//                             <MenubarTrigger>View</MenubarTrigger>
-//                             <MenubarContent>
-//                                 <MenubarCheckboxItem>Always Show Bookmarks Bar</MenubarCheckboxItem>
-//                                 <MenubarCheckboxItem checked>
-//                                     Always Show Full URLs
-//                                 </MenubarCheckboxItem>
-//                                 <MenubarSeparator />
-//                                 <MenubarItem inset>
-//                                     Reload <MenubarShortcut>⌘R</MenubarShortcut>
-//                                 </MenubarItem>
-//                                 <MenubarItem disabled inset>
-//                                     Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
-//                                 </MenubarItem>
-//                                 <MenubarSeparator />
-//                                 <MenubarItem inset>Toggle Fullscreen</MenubarItem>
-//                                 <MenubarSeparator />
-//                                 <MenubarItem inset>Hide Sidebar</MenubarItem>
-//                             </MenubarContent>
-//                         </MenubarMenu>
-//                         <MenubarMenu>
-//                             <MenubarTrigger>Profiles</MenubarTrigger>
-//                             <MenubarContent>
-//                                 <MenubarRadioGroup value="benoit">
-//                                     <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
-//                                     <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
-//                                     <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
-//                                 </MenubarRadioGroup>
-//                                 <MenubarSeparator />
-//                                 <MenubarItem inset>Edit...</MenubarItem>
-//                                 <MenubarSeparator />
-//                                 <MenubarItem inset>Add Profile...</MenubarItem>
-//                             </MenubarContent>
-//                         </MenubarMenu>
-//                     </Menubar>
-//                 </div>
-//             </div>
-//         </>
-//     );
-// };
-
 export default MenuBar;
-
-  
